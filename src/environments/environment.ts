ad59df23bfdb208896323698c38f55aee272abe1
Detournay Timothy <timothydetournay@gmail.com>
2022-09-27 04:20:34 +0200

    update to work on pc

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebaseConfig: {
+    apiKey: "AIzaSyCxuoR_VRFLp7kHOZyrWjNc_wWtC-PYii0",
+    authDomain: "pokemon-b9d1a.firebaseapp.com",
+    databaseURL: "https://pokemon-b9d1a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "pokemon-b9d1a",
+    storageBucket: "pokemon-b9d1a.appspot.com",
+    messagingSenderId: "946402382824",
+    appId: "1:946402382824:web:a44098a4b96a1200a68a5d",
+    measurementId: "G-KBM1RWV400"
+  }
 };
 
 /*
